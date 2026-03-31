b0ebab237dd5598f406b67ec3622132a0808f548
JorgeLTC <myciudadzacatecas@gmail.com>
2026-03-30 21:17:56 -0600

    Added edit task

--- todo.py
+++ todo.py
@@ -9,6 +9,10 @@ def remove_task(task):
     tasks.remove(task)
 
 
+def edit_task(task, new_task):
+    tasks[tasks.index(task)] = new_task
+
+
 def list_tasks():
     for task in tasks:
         print(task)
@@ -20,7 +24,8 @@ if __name__ == "__main__":
         print("1. Add task")
         print("2. Remove task")
         print("3. List tasks")
-        print("4. Exit")
+        print("4. Edit task")
+        print("5. Exit")
         choice = int(input("Enter your choice: "))
         if choice == 1:
             task = input("Enter task: ")
@@ -31,4 +36,8 @@ if __name__ == "__main__":
         elif choice == 3:
             list_tasks()
         elif choice == 4:
+            task = input("Enter task: ")
+            new_task = input("Enter new task: ")
+            edit_task(task, new_task)
+        elif choice == 5:
             exit = True
